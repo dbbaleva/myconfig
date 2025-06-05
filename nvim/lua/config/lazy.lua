30092b3a9530e5133b117ee4120cf61b5bba7d04
David Baleva <david.baleva@ing.com>
2025-06-05 11:11:51 +0200

    Updated extras

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.illuminate" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.elixir" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.omnisharp" },
